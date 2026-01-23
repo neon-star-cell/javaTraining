ab967bfa6924ea30dbecbf198f5467aa2b7068dc
Avnitt <trading.avnitt@gmail.com>
2026-01-23 10:38:45 +0530

    typecasting, upcasting, downcasting

--- oops/src/main/java/com/ibm/oops/Application.java
+++ oops/src/main/java/com/ibm/oops/Application.java
@@ -1,5 +1,10 @@
 package com.ibm.oops;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
 import com.ibm.oops.payroll.Employee;
 import com.ibm.oops.payroll.PermanentEmployee;
 
@@ -9,5 +14,18 @@ public class Application
 	{
 		Employee emp = new PermanentEmployee();
 		emp.netPay();
+		
+		float f = (float)3.0;
+		
+		Calendar calendar = new GregorianCalendar();
+		System.out.println(calendar.getTimeZone());
+		
+		GregorianCalendar gregorianCalendar = (GregorianCalendar)calendar;
+		System.out.println(gregorianCalendar.getCalendarType());
+		
+		List<Integer> list = new ArrayList<Integer>();
+		if(list instanceof ArrayList) {
+			ArrayList<Integer> array = (ArrayList<Integer>)list;
+		}
 	}
 }
