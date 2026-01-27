41a45a3c6919947a97942fbf1a19100eb5313c45
Avnitt <trading.avnitt@gmail.com>
2026-01-27 14:05:10 +0530

    object class methods

--- ibm/src/main/java/com/ibm/Application.java
+++ ibm/src/main/java/com/ibm/Application.java
@@ -29,6 +29,16 @@ class Point {
 	public String toString() {
 		return "(" + this.x + ", " + this.y + ")";
 	}
+	
+	@Override
+	public boolean equals(Object target) {
+		// TODO Auto-generated method stub
+		if(target != null && target instanceof Point) {
+			Point point2 = (Point)target;
+			return this.x == point2.x && this.y == point2.y;
+		}
+		return false;
+	}
 }
 
 public class Application { 
@@ -39,6 +49,8 @@ public class Application {
 		points[0][0] = new Point(0, 5);
 		points[1][0] = new Point(1, 7);
 		points[2][1] = new Point(3, 5);
+		
+		System.out.println(points[0][0].equals(points[1][0]));
 
 		System.out.printf("%.2f\n", points[0][0].distFromOrigin());
 		System.out.printf("%.2f", points[0][0].distFromPoint(points[1][0]));
