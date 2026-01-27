8b2de8628622f0db8df666367849fc44ffc89331
Avnitt <trading.avnitt@gmail.com>
2026-01-27 11:23:43 +0530

    user-defiend exceptions, throws, throw

--- exceptions/src/main/java/com/ibm/exceptions/Application.java
+++ exceptions/src/main/java/com/ibm/exceptions/Application.java
@@ -1,17 +1,29 @@
 package com.ibm.exceptions;
 
+class IllegalAgeExecption extends Exception {
+	@Override
+	public String getMessage() {
+		return "You need to be 18 or above to register for voting";
+	}
+}
+
+class Voting {
+	void register(int age) throws IllegalAgeExecption {
+		if(age < 18) {
+			throw new IllegalAgeExecption();
+		}
+	}
+}
+
 public class Application
 {
 	public static void main( String[] args )
 	{
+		Voting voting = new Voting();
 		try {
-			System.out.println(Integer.parseInt(args[0]) / Integer.parseInt(args[1]));
-		} catch (ArrayIndexOutOfBoundsException | NumberFormatException exception) {
-			System.out.println("You need to provide 2 valid numbers");
-		} catch (ArithmeticException exception) {
-			System.out.println("You can't divide by zero");
-		} catch (Exception exception) {
-			System.out.println("Unknown exception");
+			voting.register(17);
+		} catch (IllegalAgeExecption exception) {
+			exception.printStackTrace();
 		}
 		
 		System.out.println("The End");
