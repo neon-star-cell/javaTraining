bdaedb30c7a5bab8a07b7390af80f66754708324
Avnitt <trading.avnitt@gmail.com>
2026-01-28 16:42:59 +0530

    used hebrew for reader and writer

--- iostream/src/main/java/com/ibm/iostream/Application.java
+++ iostream/src/main/java/com/ibm/iostream/Application.java
@@ -1,24 +1,19 @@
 package com.ibm.iostream;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Application {
 	public static void main(String[] args) {
-		try (FileReader in = new FileReader("file1.txt")) {
-			try (FileWriter out = new FileWriter("file3.txt")) {
-				byte c;
+		try (FileReader inputStream = new FileReader("file1.txt");
+				FileWriter outputStream = new FileWriter("file2.txt");) {
+			int c;
 
-				while ((c = (byte) in.read()) != -1) {
-					out.write(c);
-				}
+			while ((c = inputStream.read()) != -1) {
+				outputStream.write(c);
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
