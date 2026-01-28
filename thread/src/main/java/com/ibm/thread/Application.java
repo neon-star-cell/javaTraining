89550769fcd3ec40fdd5333aaab39a60d8af1d7e
Avnitt <trading.avnitt@gmail.com>
2026-01-28 11:32:37 +0530

    Executors and thread pools

--- thread/src/main/java/com/ibm/thread/Application.java
+++ thread/src/main/java/com/ibm/thread/Application.java
@@ -1,5 +1,8 @@
 package com.ibm.thread;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 class Runner implements Runnable {
 	@Override
 	public void run() {
@@ -13,26 +16,40 @@ class Runner implements Runnable {
 	}
 }
 
+class Task implements Runnable {
+	int count;
+	
+	public Task(int count) {
+		this.count = count;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println(Thread.currentThread() + " --> " + count);
+	}
+}
+
 public class Application
 {
 	public static void main( String[] args )
 	{
-		Runner runner = new Runner();
-		Thread producer = new Thread(runner, "Producer");
-		Thread consumer = new Thread(runner, "Consumer");
-//		System.out.println(producer);
-//		System.out.println(producer.getState());
-		
-		producer.start();
-		consumer.start();
-		
-		try {
-			producer.join();
-			consumer.join();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		Runner runner = new Runner();
+//		Thread producer = new Thread(runner, "Producer");
+//		Thread consumer = new Thread(runner, "Consumer");
+////		System.out.println(producer);
+////		System.out.println(producer.getState());
+//		
+//		producer.start();
+//		consumer.start();
+//		
+//		try {
+//			producer.join();
+//			consumer.join();
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
 		
 //		System.out.println(producer.getState());
@@ -44,8 +61,14 @@ public class Application
 //			e.printStackTrace();
 //		}
 		
-		System.out.println(producer.getState());
-		System.out.println(consumer.getState());
+//		System.out.println(producer.getState());
+//		System.out.println(consumer.getState());
+		
+		ExecutorService service = Executors.newFixedThreadPool(4);
+		
+		for(int counter = 0; counter < 10; counter++) {
+			service.submit(new Task(counter));
+		}
 		System.out.println("The End");
 	}
 }
