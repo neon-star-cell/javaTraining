7246c23f4043b7e21fec35e5a7c7ce6905cbe9c4
Avnitt <trading.avnitt@gmail.com>
2026-01-28 16:27:39 +0530

    FileReader and FileWriter

--- iostream/src/main/java/com/ibm/iostream/Application.java
+++ iostream/src/main/java/com/ibm/iostream/Application.java
@@ -3,43 +3,24 @@ package com.ibm.iostream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
-public class Application
-{
-	public static void main( String[] args )
-	{
-		FileInputStream in = null;
-		FileOutputStream out = null;
-		
-		try {
-			in = new FileInputStream("file1.txt");
-			out = new FileOutputStream("file2.txt");
-			byte c;
-			
-			while((c = (byte)in.read()) != -1) {
-				out.write(c);
+public class Application {
+	public static void main(String[] args) {
+		try (FileReader in = new FileReader("file1.txt")) {
+			try (FileWriter out = new FileWriter("file3.txt")) {
+				byte c;
+
+				while ((c = (byte) in.read()) != -1) {
+					out.write(c);
+				}
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			if(in != null) {
-				try {
-					in.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			if(out != null) {
-				try {
-					out.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
 		}
 	}
 }
