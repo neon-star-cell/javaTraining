35dad49646444d35595dd5d665502c8e3a4deee4
Avnitt <trading.avnitt@gmail.com>
2026-01-28 13:06:31 +0530

    lambda expressions basics

--- lambdas/src/main/java/com/ibm/lambdas/Application.java
+++ lambdas/src/main/java/com/ibm/lambdas/Application.java
@@ -1,17 +1,34 @@
 package com.ibm.lambdas;
 
-public class Application
-{
-	public static void main( String[] args )
-	{
-		Thread thread1 = new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				System.out.println(Thread.currentThread());		
-			}
-		});
+interface Printer {
+	void printing();
+}
+
+class Process {
+	public void execute(Printer printer) {
+		printer.printing();
+	}
+}
+
+public class Application {
+	public static void main(String[] args) {
+//		Thread thread1 = new Thread(new Runnable() {
+//			
+//			@Override
+//			public void run() {
+//				System.out.println(Thread.currentThread());		
+//			}
+//		});
+//		
+//		thread1.start();
+		Process process = new Process();
+//		process.execute(new Printer() {
+//			public void printing() {
+//				System.out.println("Printing");
+//			}
+//		});
 		
-		thread1.start();
+		process.execute(() -> System.out.println("HP printer printing"));
+
 	}
 }
