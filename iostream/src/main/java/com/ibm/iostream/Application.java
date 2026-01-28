6792bf9293904ff547e56608adce7c217296faeb
Avnitt <trading.avnitt@gmail.com>
2026-01-28 17:00:59 +0530

    writing binary and stream chaining

--- iostream/src/main/java/com/ibm/iostream/Application.java
+++ iostream/src/main/java/com/ibm/iostream/Application.java
@@ -1,20 +1,22 @@
 package com.ibm.iostream;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Application {
 	public static void main(String[] args) {
-		try (FileReader inputStream = new FileReader("file1.txt");
-				FileWriter outputStream = new FileWriter("file2.txt");) {
-			int c;
-
-			while ((c = inputStream.read()) != -1) {
-				outputStream.write(c);
-			}
+		try (DataOutputStream out = new DataOutputStream(
+				new BufferedOutputStream(new FileOutputStream("./src/main/resources/ibm/bifile1.txt")))) {
+			
+			out.writeShort(12345);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
